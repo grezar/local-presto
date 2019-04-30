37663b9ee6d604c89b91ae112113a4f58cbef7a2
grezar <grezar.dev@gmail.com>
2019-04-30 17:08:27 +0900

    Split sql directory for each database

--- migrate_and_seed.sh
+++ migrate_and_seed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "migrating..."
-mysql -u root -P 33061 -h 127.0.0.1 < sql/migration/*.sql
-mysql -u root -P 33062 -h 127.0.0.1 < sql/migration/*.sql
+mysql -u root -P 33061 -h 127.0.0.1 < sql/first_mysql/migration/*.sql
+mysql -u root -P 33062 -h 127.0.0.1 < sql/second_mysql/migration/*.sql
 
 echo "seeding..."
-mysql -u root -P 33061 -h 127.0.0.1 < sql/seed/*.sql
-mysql -u root -P 33062 -h 127.0.0.1 < sql/seed/*.sql
+mysql -u root -P 33061 -h 127.0.0.1 < sql/first_mysql/seed/*.sql
+mysql -u root -P 33062 -h 127.0.0.1 < sql/second_mysql/seed/*.sql
 
 echo "finish"
---
+++ sql/second_mysql/seed/items.sql
@@ -0,0 +1,11 @@
+INSERT INTO presto_sandbox.items (name, description, price, created_at) VALUES
+('admiring',   'name starts with a', 100,  CURRENT_TIMESTAMP),
+('beautiful',  'name starts with b', 200,  CURRENT_TIMESTAMP),
+('charming',   'name starts with c', 300,  CURRENT_TIMESTAMP),
+('dazzling',   'name starts with d', 400,  CURRENT_TIMESTAMP),
+('eager',      'name starts with e', 500,  CURRENT_TIMESTAMP),
+('fervent',    'name starts with f', 600,  CURRENT_TIMESTAMP),
+('gallant',    'name starts with g', 700,  CURRENT_TIMESTAMP),
+('happy',      'name starts with h', 800,  CURRENT_TIMESTAMP),
+('infallible', 'name starts with i', 900,  CURRENT_TIMESTAMP),
+('joyful',     'name starts with j', 1000, CURRENT_TIMESTAMP);
