e0587072b3b7ea05850c3effb07ed1029da765cb
grezar <grezar.dev@gmail.com>
2019-04-30 16:51:26 +0900

    Also second_mysql

--- migrate_and_seed.sh
+++ migrate_and_seed.sh
@@ -2,8 +2,10 @@
 
 echo "migrating..."
 mysql -u root -P 33061 -h 127.0.0.1 < sql/migration/*.sql
+mysql -u root -P 33062 -h 127.0.0.1 < sql/migration/*.sql
 
 echo "seeding..."
 mysql -u root -P 33061 -h 127.0.0.1 < sql/seed/*.sql
+mysql -u root -P 33062 -h 127.0.0.1 < sql/seed/*.sql
 
 echo "finish"
